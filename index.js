e02b34b1ab2e2e2804fb103124dcf04d1be1422f
ORiveraJr84 <oriverajr84@gmail.com>
2020-07-31 23:39:21 -0400

    Just got 4 left.

--- index.js
+++ index.js
@@ -208,12 +208,13 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
-console.log("This is the first artist's name ---> " + artists[0].name);
-console.log("This is the third artist's bio ---> " + artists[2].bio);
+console.log("This is the answer to task 1A -- first artist's name ---> " + artists[0].name);
+console.log("This is the answer to task 1B -- the third artist's bio ---> " + artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent Van Gogh";
+console.log("This is the answer to task 2 -- the 9th artist's corrected name ---> " + artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -224,19 +225,28 @@ console.log("This is the third artist's bio ---> " + artists[2].bio);
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    console.log (`This is the answer to task 3 ---> The artist at index ${index} is ${array[index].name}.`);
   }
+
+  getArtistByIndex(artists,0);
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
+function get20s(array){
 
-  /* Code here */
+  let filteredArr = [];
+
+  
+  let splitArray = array[i].years.split(" ");
+
+
 
 }
+
+get20s(artists);
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -249,12 +259,13 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
+function removeArtist(array,index) {
+  array.splice(index,1);
+  console.log("This is the answer to task 5 ---> " , array);
+}
   
+removeArtist(artists,1);
  
-
 /**
 /* Task 6: Create a function called `addArtist` that can accept an object of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
 
@@ -268,11 +279,22 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
-
+function addArtist(id, name, years, genre, nationality, bio){
+  
+  let newArtist = {
+    "id": id,
+    "name": name,
+    "years": years,
+    "genre": genre,
+    "nationality": nationality,
+    "bio": bio
   }
+
+  artists.push(newArtist);
+  console.log("This is the answer to task 6 ---> ", artists);
+}
+
+addArtist(20, "Orlando Rivera, Jr.", "1984 - present", "Web Design", "Puerto Rican","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, dui ut tempor condimentum, mi neque sollicitudin leo, interdum dictum dui dui fringilla ante. Mauris vel placerat metus. Etiam rhoncus fringilla ultrices. Pellentesque eleifend massa eu dolor porta, vel cursus diam lacinia. Nam vel lectus elit. Maecenas eleifend mi eu nisi luctus mattis. Nunc vel sem imperdiet, imperdiet sem sed, ornare risus.");
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -282,13 +304,21 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
+function lotsOfArt(arr){
 
-  /* Code here */
+  let filteredArr = [];
 
+  arr.forEach(element => {
+
+    if (element.paintings >= 100) {
+      filteredArr.push(element);
+    }
+    
+  });
+  console.log("This is the answer to task 7 ---> ", filteredArr);
 }
 
-
+lotsOfArt(artists); 
 
 // 🎨🎨 STRETCH 🎨🎨//
 
